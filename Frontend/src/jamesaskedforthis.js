073d1559ed457f8fc7ee6653c7040f738c6bd46a
kingminor <michaelgbrightman@yahoo.com>
2026-02-28 09:24:18 -0700

    james

--- Frontend/src/jamesaskedforthis.js
+++ Frontend/src/jamesaskedforthis.js
@@ -6,3 +6,24 @@ const options = {
         },
         body: `${campaignId}`,
     }
+
+await fetch(API_URL + "/api/character/getCampaignMembership", options);
+
+// Object Looks like this
+var object = {
+    "Id": "string",
+    "CampaignId": "string",
+    "Campaign": {
+        // Bunch of data relating to the campaign
+    },
+    "PlayerUserId": "string",
+    "PlayerUser": {
+        //Stuff for playeruser
+    },
+    "Character": {
+
+    },
+    "DateCreated": "DateTime",
+    "LastUpdated": "DateTime",
+    "IsApproved": true
+}
