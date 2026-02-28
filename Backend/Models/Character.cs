cb7c816f4c5f886a5dfa2e4f67c7a66f9928a5c3
kingminor <michaelgbrightman@yahoo.com>
2026-02-28 07:22:06 -0700

    stab

--- Backend/Models/Character.cs
+++ Backend/Models/Character.cs
@@ -16,7 +16,7 @@ public class Character
     public string? Class { get; set; }
     public string? Race { get; set; }
     public string? Background { get; set; }
-    public Alignment? Alignment { get; set; }
+    public string? Alignment { get; set; }
     public string? PersonalityTraits { get; set; }
     public string? Ideals  { get; set; }
     public string? Bonds  { get; set; }
