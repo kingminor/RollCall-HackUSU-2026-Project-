703dbe1fc35ff2d6823cf02c02be8b8ba3dbec18
kingminor <michaelgbrightman@yahoo.com>
2026-02-27 22:31:52 -0700

    stuff

--- Backend/ApplicationDbContext.cs
+++ Backend/ApplicationDbContext.cs
@@ -14,4 +14,18 @@ public class ApplicationDbContext : IdentityDbContext<PlayerUser>
     public DbSet<Campaign> Campaigns { get; set; }
     public DbSet<CampaignMembership>  CampaignMemberships { get; set; }
     public DbSet<Character> Characters { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Campaign>()
+            .HasIndex(u => u.AlphaNumericJoinCode)
+            .IsUnique();
+        
+        modelBuilder.Entity<Campaign>()
+            .HasIndex(u => u.id)
+            .IsUnique();
+
+    }
 }
